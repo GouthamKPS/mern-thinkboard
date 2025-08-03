500d8355ec9351f1af0699cc16bd0fd8b4aa6686
GouthamKPS <gouthamkrishnahere@gmail.com>
2025-08-03 22:35:08 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import dotenv from "dotenv"
+import cors from 'cors'
+
+import notesRoutes from './routes/notesRoutes.js'
+import { connectDB } from './config/db.js'
+import rateLimiter from './middleware/rateLimiter.js';
+
+dotenv.config();
+
+const app = express()
+const PORT = process.env.PORT || 5001;
+
+connectDB().then(()=>{
+app.listen(PORT,()=>{
+    console.log("Server started on PORT:",PORT);
+});
+});
+app.use(cors())
+app.use(express.json());
+app.use(rateLimiter);
+app.use("/api/notes",notesRoutes);
+
