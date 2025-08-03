103df9e9a08937f67aa9548ae83c39ae559fe6dd
GouthamKPS <gouthamkrishnahere@gmail.com>
2025-08-03 23:06:41 +0530

    prepared for render deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from "dotenv"
 import cors from 'cors'
+import path from 'path'
 
 import notesRoutes from './routes/notesRoutes.js'
 import { connectDB } from './config/db.js'
@@ -16,8 +17,19 @@ app.listen(PORT,()=>{
     console.log("Server started on PORT:",PORT);
 });
 });
-app.use(cors())
+
+if(process.env.NODE_ENV!=="production"){
+    app.use(cors())
+}
 app.use(express.json());
 app.use(rateLimiter);
 app.use("/api/notes",notesRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+})
+}
 
